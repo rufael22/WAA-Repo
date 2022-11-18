0b0e77921fee81387ff33409fddde78d31be06db
Rufael Yohannes <rafigitom@gmail.com>
2022-11-18 14:24:56 -0600

    lab-9

---
+++ Lab-9/lab9/src/App.js
@@ -0,0 +1,11 @@
+import Dashboard from './containers/Dashboard/Dashboard'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+};
+
+export default App;
